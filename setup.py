cf59f7555f99e2676e9ebc7dfa61384335bdab87
Bryan Chan <bryanchan13@gmail.com>
2022-12-29 20:03:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license=None,
-    url="http://github.com/chanb/cmput676_winter2022",
+    url="http://github.com/chanb/prophet_inequality",
 )
